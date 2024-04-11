492893242e738e03bed0917fd0e3119a1618f97f
Maxwell Siqueira <56099524+maaxsiq@users.noreply.github.com>
2024-04-11 14:16:48 -0300

    Update index.js

--- src/pages/Details/index.js
+++ src/pages/Details/index.js
@@ -2,18 +2,17 @@ import { useRoute } from '@react-navigation/native';
 import React from 'react';
 import { View } from 'react-native';
 
- import { Container,Title,Interest} from './styles';
+import { Container,Title,Interest} from './styles';
 
 const Details = () => {
-    const route = useRoute()
+  const route = useRoute()
   return(
         <Container>
-         <Title>Name: {route.params.item.name}</Title>
-         <Title>Interest:</Title>
-         {route.params.item.interest.map(item=>(
-            <Interest>{item}</Interest>
-         ))}
-
+          <Title>Name: {route.params.item.name}</Title>
+          <Title>Interest:</Title>
+          {route.params.item.interest.map(item=>(
+             <Interest>{item}</Interest>
+          ))}
         </Container>
   );
 }
